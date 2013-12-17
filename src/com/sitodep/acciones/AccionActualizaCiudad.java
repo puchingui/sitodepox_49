79b41eaae8d11b4dfd9e5b5363e13ca48efb7ac9
Kenneth Burgos <kenneth.burgos@gmail.com>
2013-12-16 23:29:24 -0400

    Probando @OnChange

---
+++ src/com/sitodep/acciones/AccionActualizaCiudad.java
@@ -0,0 +1,12 @@
+package com.sitodep.acciones;
+
+import org.openxava.actions.*;
+
+public class AccionActualizaCiudad extends OnChangePropertyBaseAction {
+
+	public void execute() throws Exception {
+		if (getNewValue() == null) return;
+		
+	}
+
+}
